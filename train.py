e010b3bea28d655543f18423ce8efcdf8c00371d
SU Zhenxuan <sinesu@sjtu.edu.cn>
2022-01-08 15:18:06 +0800

    change input to full person image

--- train.py
+++ train.py
@@ -67,7 +67,8 @@ def train_gmm(opt, train_loader, model, board):
         im_c =  inputs['parse_cloth'].cuda()
         im_g = inputs['grid_image'].cuda()
             
-        grid, theta = model(agnostic, c)
+        # grid, theta = model(agnostic, c)
+        grid, theta, VIB_loss = model(im, c)
         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
         warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
@@ -75,8 +76,8 @@ def train_gmm(opt, train_loader, model, board):
         visuals = [ [im_h, shape, im_pose], 
                    [c, warped_cloth, im_c], 
                    [warped_grid, (warped_cloth+im)*0.5, im]]
-        
-        loss = criterionL1(warped_cloth, im_c)    
+        beta = 0.000
+        loss = criterionL1(warped_cloth, im_c) + beta * VIB_loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -85,7 +86,7 @@ def train_gmm(opt, train_loader, model, board):
             board_add_images(board, 'combine', visuals, step+1)
             board.add_scalar('metric', loss.item(), step+1)
             t = time.time() - iter_start_time
-            print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()), flush=True)
+            print('step: %8d, time: %.3f, loss: %4f, L1: %4f, VIB: %4f' % (step+1, t, loss.item(), loss.item() - beta * VIB_loss, VIB_loss), flush=True)
 
         if (step+1) % opt.save_count == 0:
             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
