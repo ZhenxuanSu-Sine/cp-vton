9f0921b346a5b112ad176f75fbbda7c175beb351
Sine <sinesu@sjtu.edu.cn>
2022-01-13 00:53:34 +0800

    test shell script

--- test.py
+++ test.py
@@ -54,6 +54,8 @@ def test_gmm(opt, test_loader, model, board):
     if not os.path.exists(warp_mask_dir):
         os.makedirs(warp_mask_dir)
 
+    criterionL1 = nn.L1Loss()
+
     for step, inputs in enumerate(test_loader.data_loader):
         iter_start_time = time.time()
         
@@ -68,7 +70,7 @@ def test_gmm(opt, test_loader, model, board):
         im_c =  inputs['parse_cloth'].cuda()
         im_g = inputs['grid_image'].cuda()
             
-        # grid, theta = model(agnostic, c)
+        # grid, theta, VIB_loss = model(agnostic, c)
         grid, theta, VIB_loss = model(im, c)
         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
