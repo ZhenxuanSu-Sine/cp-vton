1b4cc4d7ee6066c1c933cb52a1f8aea324ddc5a3
Sine <sinesu@sjtu.edu.cn>
2022-01-13 01:07:33 +0800

    model with reparametrization

--- train.py
+++ train.py
@@ -69,8 +69,8 @@ def train_gmm(opt, train_loader, model, board):
         im_c =  inputs['parse_cloth'].cuda()
         im_g = inputs['grid_image'].cuda()
             
-        # grid, theta = model(agnostic, c)
-        grid, theta, VIB_loss = model(im, c)
+        grid, theta = model(agnostic, c)
+        # grid, theta, VIB_loss = model(im, c)
         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
         warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
